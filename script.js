f008439d5c8169c1f89217a4b1e6d419fe6c47a2
99063871 <99063871@mydavinci.com>
2022-01-19 12:51:23 +0100

    switchlight in javascript gemaakt

--- script.js
+++ script.js
@@ -4,7 +4,18 @@ btn.innerHTML = '...';
 document.body.appendChild(btn);
 
 // schijf hier tussen je code
+btn.innerHTML = "Switch light on";
+document.body.style.backgroundColor = "black";
 
+btn.onclick = function(){
+    if(btn.innerHTML == "Switch light on"){
+        btn.innerHTML = "Switch light off"
+        document.body.style.backgroundColor = "yellow";
+    }else{
+        btn.innerHTML = "Switch light on"
+        document.body.style.backgroundColor = "black";}
+        
+};
 
 
 // schijf hier tussen je code
