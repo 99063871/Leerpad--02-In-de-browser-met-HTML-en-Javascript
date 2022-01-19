9ab1f638c2377342cd165c3d576d0ddf537e397b
99063871 <99063871@mydavinci.com>
2022-01-19 12:58:34 +0100

    console.log toegevoegd(weinig commits want ik heb het in 1 reeks gemaakt door kennis vorig jaar)

--- script.js
+++ script.js
@@ -11,9 +11,12 @@ btn.onclick = function(){
     if(btn.innerHTML == "Switch light on"){
         btn.innerHTML = "Switch light off"
         document.body.style.backgroundColor = "yellow";
+        console.log("Light is on")
     }else{
         btn.innerHTML = "Switch light on"
-        document.body.style.backgroundColor = "black";}
+        document.body.style.backgroundColor = "black";
+        console.log("Light is off")
+    }
         
 };
 
